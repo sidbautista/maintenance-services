70cdd0a62ae9c6434728c0d4eb190a5cf6e0dfa2
Sid Bautista <mvsgbautista@iskolarngbayan.pup.edu.ph>
2023-08-18 15:12:57 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UtilitiesComponent } from './utilities/utilities.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './utilities/navbar/navbar.component';
+import { FooterComponent } from './utilities/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilitiesComponent,
+    HomePageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
