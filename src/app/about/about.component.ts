2eba4f25ac9e46d13fe22f2d3611f7408cbeecfa
Sid Bautista <mvsgbautista@iskolarngbayan.pup.edu.ph>
2023-08-31 19:15:09 +0800

    style(component): :lipstick: apply wowjs animate css in about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as WOW from 'wow.js';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit  {
+ ngOnInit() {
+    // Initialize WOW
+    const wow = new (WOW as any)({
+      mobile: false,
+    });
+    wow.init();
+  }
 }
